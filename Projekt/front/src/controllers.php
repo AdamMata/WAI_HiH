f4d95da00b31046a4c87a5d1f6a1555c45a54022
AdamMata <amatczak2k5@gmail.com>
2023-12-14 16:16:27 +0100

    added controllers.php

---
+++ Projekt/front/src/controllers.php
@@ -0,0 +1,12 @@
+<?php
+
+function index($model) {} // TODO : display login
+
+function collection($model) {} // TODO : display login
+
+function account($model) { // TODO : implement
+	// if not logged in
+		// log in or register
+	// else - logged in
+		// show selected items?
+}

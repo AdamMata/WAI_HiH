ad17f4759dc598f73a61fbf7d2d230c475672574
AdamMata <amatczak2k5@gmail.com>
2023-12-15 18:02:11 +0100

    adjust to mvc

--- Projekt/front/src/controllers.php
+++ Projekt/front/src/controllers.php
@@ -1,12 +1,21 @@
 <?php
 
-function index($model) {} // TODO : display login
+function index($model) {
+	return "index-view";
+} // TODO : display login
 
-function collection($model) {} // TODO : display login
+function collection($model) {
+	return "collection-view";	
+} // TODO : display login
+
+function forum($model) {
+	return "forum-view";
+}
 
 function account($model) { // TODO : implement
 	// if not logged in
 		// log in or register
 	// else - logged in
 		// show selected items?
+	return "account-view";
 }
--- Projekt/front/src/dispatcher.php
+++ Projekt/front/src/dispatcher.php
@@ -1,17 +1,20 @@
 <?php
 
+require_once 'controllers.php';
+
 const ROUTING = [
 	"/"=> "index",
 	"/collection"=> "collection",
 	"/account"=> "account",
+	"/forum"=> "forum"
 ];
 
 function dispatch($action) {
-	$controller = ROUTING["$action"];
+	$controller_fn = ROUTING[$action];
 	$model = [];
-	$view = $controller[$model];
+	$view = $controller_fn($model); // populate model using controller_fn function
 
-	if (strpos($view, 'redirect:') != 0){
+	if (strpos($view, 'redirect:') !== 0){
 		render($view, $model);
 	} else {
 		$url = substr($view, strlen('redirect:')); // redirect where?
@@ -22,5 +25,5 @@ function dispatch($action) {
 
 function render($view, $model) {
 	extract($model);
-	include 'views/'.$view.'php';
+	require 'views/'.$view.'.php';
 } // TODO : understand wtf i am doing
